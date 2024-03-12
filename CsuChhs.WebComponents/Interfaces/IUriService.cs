8f42afce9b8f084d226eba8e0390facc69e5d4c8
Derek Stegelman <dstegelman@gmail.com>
2024-03-12 14:25:29 -0600

    Remove key and pagination components

--- CsuChhs.WebComponents/Interfaces/IUriService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CsuChhs.WebComponents.Filters;
-
-namespace CsuChhs.WebComponents.Interfaces
-{
-    public interface IUriService
-    {
-        public Uri GetPageUri(PaginationFilter filter, string route);
-    }
-}
