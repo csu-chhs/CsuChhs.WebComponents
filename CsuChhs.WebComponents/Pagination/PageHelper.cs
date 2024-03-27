342e243b6f331adfefa76167e3174f2637cdebf1
Derek Stegelman <dstegelman@gmail.com>
2024-03-27 08:23:12 -0600

    Update Pagination

---
+++ CsuChhs.WebComponents/Pagination/PageHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace CsuChhs.WebComponents.Pagination
+{
+    public static class PageHelper
+    {
+        public static int CalculateSkip(int page,
+            int take)
+        {
+            return (page * take) - take;
+        }
+
+        public static int CalculateTotalPages(int totalCollectionCount,
+            int take)
+        {
+            return (totalCollectionCount + take - 1) / take;
+        }
+    }
+}
